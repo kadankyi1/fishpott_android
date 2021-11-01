f5d80d0540fcdff3623956ec26fe28097ac4ae1c
kadankyi1 <annodankyikwaku@gmail.com>
2021-11-01 15:18:06 -0700

    Created new style and new activity for findbusiness

---
+++ app/src/main/java/com/fishpott/fishpott5/Activities/FindBusinessActivity.java
@@ -0,0 +1,17 @@
+package com.fishpott.fishpott5.Activities;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+import com.fishpott.fishpott5.R;
+
+public class FindBusinessActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_find_business);
+
+
+    }
+}
