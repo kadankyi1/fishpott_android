86845248514a6d4cb37e36e905748d4f6da0c950
kadankyi1 <annodankyikwaku@gmail.com>
2022-03-11 02:27:50 -0800

    Working on xml design for personality activity

---
+++ app/src/main/java/com/fishpott/fishpott5/Activities/PersonalityActivity.java
@@ -0,0 +1,16 @@
+package com.fishpott.fishpott5.Activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.fishpott.fishpott5.R;
+
+public class PersonalityActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_personality);
+    }
+}
